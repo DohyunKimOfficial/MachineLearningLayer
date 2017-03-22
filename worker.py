c1775015581bb25b470f53c01b14f8c602388cb0
Matúš Tomlein <matus.tomlein@gmail.com>
2017-03-22 17:44:06 -0400

    Minor change

--- worker.py
+++ worker.py
@@ -2,9 +2,8 @@ from giotto.connect_to_redis import connect_to_redis
 from rq import Worker
 
 redis = connect_to_redis()
-QUEUES = ['high', 'normal', 'low']
 
 print('Starting a worker')
 
-worker = Worker(QUEUES, connection=redis)
+worker = Worker('default', connection=redis)
 worker.work(logging_level='WARNING')
